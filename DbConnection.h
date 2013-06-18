b55f211ca7ba6a2dfb3af0f9d076590605c636ad
Jeremy Trufier <jeremy@trufier.com>
2013-06-18 18:47:54 +0200

    Clear

---
+++ DbConnection.h
@@ -0,0 +1,13 @@
+//
+//  DbConnection.h
+//  Deskboard
+//
+//  Created by Jeremy Trufier on 15/06/13.
+//  Copyright (c) 2013 Jeremy Trufier. All rights reserved.
+//
+
+#import "ZIMDbConnection.h"
+
+@interface DbConnection : ZIMDbConnection
+
+@end
---
+++ DbConnection.m
@@ -0,0 +1,13 @@
+//
+//  DbConnection.m
+//  Deskboard
+//
+//  Created by Jeremy Trufier on 15/06/13.
+//  Copyright (c) 2013 Jeremy Trufier. All rights reserved.
+//
+
+#import "DbConnection.h"
+
+@implementation DbConnection
+
+@end
